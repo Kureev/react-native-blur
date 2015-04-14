acdd581e039f66bdc7b1db69b6e27fea6adf7bdf
Alexey Kureev <a.kureev@xs2theworld.com>
2015-04-14 10:49:51 +0200

    Optimize blur view

--- RNBlur/BlurView.m
+++ RNBlur/BlurView.m
@@ -1,25 +1,36 @@
 #import "BlurView.h"
 
 @implementation BlurView {
+  UIVisualEffectView *_visualEffectView;
+}
+
+- (void)setBlurType:(NSString *)blurType
+{
+  if (_visualEffectView) {
+    [_visualEffectView removeFromSuperview];
+  }
+
   UIBlurEffect *blurEffect;
-  UIVisualEffectView *visualEffectView;
+
+  if ([blurType isEqual: @"xlight"]) {
+    blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleExtraLight];
+  } else if ([blurType isEqual: @"light"]) {
+    blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
+  } else if ([blurType isEqual: @"dark"]) {
+    blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleDark];
+  } else {
+    blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleDark];
+  }
+
+  _visualEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
 }
 
 -(void)layoutSubviews
 {
   [super layoutSubviews];
-  
-  if ([_blurType isEqual: @"xlight"]) {
-    blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleExtraLight];
-  } else if ([_blurType isEqual: @"light"]) {
-    blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
-  } else if ([_blurType isEqual: @"dark"]) {
-    blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleDark];
-  }
-  
-  visualEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
-  visualEffectView.frame = self.bounds;
-  [self insertSubview:visualEffectView atIndex:0];
+
+  _visualEffectView.frame = self.bounds;
+  [self insertSubview:_visualEffectView atIndex:0];
 }
 
 @end
