37e49ca333b5b95e07f61079f3363aea00086eea
Julien K <JulienKode@users.noreply.github.com>
2017-06-27 10:13:39 +0200

    Change ifdef to if to check OS

--- ios/BlurView.m
+++ ios/BlurView.m
@@ -54,7 +54,7 @@
   if ([self.blurType isEqual: @"light"]) return UIBlurEffectStyleLight;
   if ([self.blurType isEqual: @"dark"]) return UIBlurEffectStyleDark;
     
-  #ifdef TARGET_OS_TV
+  #if TARGET_OS_TV
     if ([self.blurType isEqual: @"extraDark"]) return UIBlurEffectStyleExtraDark;
     if ([self.blurType isEqual: @"regular"]) return UIBlurEffectStyleRegular;
     if ([self.blurType isEqual: @"prominent"]) return UIBlurEffectStyleProminent;
