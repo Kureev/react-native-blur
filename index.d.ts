db215a9e2262cea119f8dd1cd555c3b17557fc2a
Antoine Hanriat <ahanriat@gmail.com>
2017-08-07 10:57:25 +0200

    Add Typescript definitions (#225)
    * Add Typescript definitions
    
    * Fix typescript definition

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { ViewStyle } from 'react-native';
+
+export interface BlurViewProperties {
+  blurType: 'xlight' | 'light' | 'dark'
+    // tvOS only
+    | 'extraDark' | 'regular' | 'prominent';
+  blurAmount?: number; // 0 - 100
+  style?: ViewStyle;
+}
+
+export class BlurView extends React.Component<BlurViewProperties, {}> {}
+
+
+export interface VibrancyViewProperties extends BlurViewProperties {}
+
+export class VibrancyView extends React.Component<VibrancyViewProperties, {}> {}
