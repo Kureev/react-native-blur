60cc1f611fbd660fb8b545630c45d7dd688f1272
Alexey <a.g.kureev@gmail.com>
2017-08-08 11:58:32 +0200

    Update BlurViewPackage.java

--- android/src/main/java/com/cmcewen/blurview/BlurViewPackage.java
+++ android/src/main/java/com/cmcewen/blurview/BlurViewPackage.java
@@ -20,7 +20,6 @@ public class BlurViewPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
