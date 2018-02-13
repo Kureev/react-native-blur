1e4200c1938ffbef1fef625e1f4947646de3fcd4
Bartol Karuza <bartol@immidi.com>
2018-02-13 15:59:22 +0100

    The findViewById originally used can often not find the actual view it needs to blur. To find every view consistently, the NativeViewHierarchyManager is used to convert the view tag to the view id.

--- android/src/main/java/com/cmcewen/blurview/BlurViewManager.java
+++ android/src/main/java/com/cmcewen/blurview/BlurViewManager.java
@@ -1,10 +1,17 @@
 package com.cmcewen.blurview;
 
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.View;
 
+import com.facebook.react.ReactApplication;
+import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.uimanager.NativeViewHierarchyManager;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.UIBlock;
+import com.facebook.react.uimanager.UIManagerModule;
 import com.facebook.react.uimanager.annotations.ReactProp;
 
 
@@ -49,13 +56,28 @@ public class BlurViewManager extends SimpleViewManager<BlurringView> {
     }
 
     @ReactProp(name = "viewRef")
-    public void setViewRef(BlurringView view, int viewRef) {
+    public void setViewRef(final BlurringView view, final int viewRef) {
         if (context != null && context.getCurrentActivity() != null) {
-          View viewToBlur = context.getCurrentActivity().findViewById(viewRef);
-
-          if (viewToBlur != null) {
-              view.setBlurredView(viewToBlur);
-          }
+            View viewToBlur = context.getCurrentActivity().findViewById(viewRef);
+            if(viewToBlur != null) {
+                view.setBlurredView(viewToBlur);
+                return;
+            }
+            context.getCurrentActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    UIManagerModule uiManager = context.getNativeModule(UIManagerModule.class);
+                    uiManager.addUIBlock(new UIBlock() {
+                        @Override
+                        public void execute(NativeViewHierarchyManager nativeViewHierarchyManager) {
+                            View viewToBlur = nativeViewHierarchyManager.resolveView(viewRef);
+                            if (viewToBlur != null) {
+                                view.setBlurredView(viewToBlur);
+                            }
+                        }
+                    });
+                };
+            });
         }
     }
 }
