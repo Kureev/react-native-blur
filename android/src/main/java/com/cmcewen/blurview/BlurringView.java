4a9203e48793790c3c18a14dc7b71fc79236cd0b
eladgel <elad@honeybook.com>
2019-07-31 16:11:06 -0700

    Update BlurringView.java
    work with androidx

--- android/src/main/java/com/cmcewen/blurview/BlurringView.java
+++ android/src/main/java/com/cmcewen/blurview/BlurringView.java
@@ -7,10 +7,10 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.support.v8.renderscript.Allocation;
-import android.support.v8.renderscript.Element;
-import android.support.v8.renderscript.RenderScript;
-import android.support.v8.renderscript.ScriptIntrinsicBlur;
+import androidx.renderscript.Allocation;
+import androidx.renderscript.Element;
+import androidx.renderscript.RenderScript;
+import androidx.renderscript.ScriptIntrinsicBlur;
 import android.util.AttributeSet;
 import android.view.View;
 
