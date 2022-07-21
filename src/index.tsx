521de2a14d2576fcda71d7963d043969dd3abebd
Thibault Malbranche <malbranche.thibault@gmail.com>
2022-07-21 13:02:30 +0200

    fix types

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
-import BlurView from './components/BlurView';
-import VibrancyView from './components/VibrancyView';
+import BlurViewUntyped from './components/BlurView';
+import VibrancyViewUntyped from './components/VibrancyView';
+import type { View } from 'react-native'
 
-import type { BlurViewProps } from './components/BlurView';
-import type { VibrancyViewProps } from './components/VibrancyView';
+import type { BlurViewProps as BlurViewPropsIOS } from './components/BlurView.ios';
+import type { BlurViewProps as BlurViewPropsAndroid } from './components/BlurView.android';
+import type { VibrancyViewProps as VibrancyViewPropsIOS } from './components/VibrancyView.ios';
+
+type BlurViewProps = BlurViewPropsIOS | BlurViewPropsAndroid;
+type VibrancyViewProps = VibrancyViewPropsIOS;
+
+const BlurView = BlurViewUntyped as React.ForwardRefExoticComponent<BlurViewProps & React.RefAttributes<View>>
+const VibrancyView = VibrancyViewUntyped as React.ForwardRefExoticComponent<VibrancyViewProps & React.RefAttributes<View>>
 
 export { BlurView, VibrancyView };
 export type { BlurViewProps, VibrancyViewProps };
