681a65bc777a05a8aa536b2f73fc99bf452a3202
Thibault Malbranche <malbranche.thibault@gmail.com>
2023-05-03 19:01:02 +0200

    fix: tvOS build fix

--- ios/BlurView.mm
+++ ios/BlurView.mm
@@ -139,8 +139,8 @@ using namespace facebook::react;
     if ([self.blurType isEqual: @"regular"]) return UIBlurEffectStyleRegular;
     if ([self.blurType isEqual: @"prominent"]) return UIBlurEffectStyleProminent;
   #endif
-    
-  #if defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 130000 /* __IPHONE_13_0 */
+
+  #if !TARGET_OS_TV && defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 130000 /* __IPHONE_13_0 */
     // Adaptable blur styles
     if ([self.blurType isEqual: @"chromeMaterial"]) return UIBlurEffectStyleSystemChromeMaterial;
     if ([self.blurType isEqual: @"material"]) return UIBlurEffectStyleSystemMaterial;
--- react-native-blur.podspec
+++ react-native-blur.podspec
@@ -12,7 +12,7 @@ Pod::Spec.new do |s|
   s.license      = package["license"]
   s.authors      = package["author"]
 
-  s.platforms    = { :ios => "10.0" }
+  s.platforms    = { :ios => "10.0", :tvos => "11.0" }
   s.source       = { :git => "https://github.com/react-native-community/react-native-blur.git" }
 
   s.source_files = "ios/**/*.{h,m,mm}"
